c148ef86ab41e0082841540ea55798febc9b84a3
Jesson <6838165+jessonfoo@users.noreply.github.com>
2022-03-18 16:56:07 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,40 @@
-#!/bin/bash
-declare -A deps;
+#!/bin/sh
 
-deps=(git, ruby, rake, vim, fontconfig, tmux, tree, htop, lnav)
 if [ ! -d "$HOME/.gzn" ]; then
 	platform='unknown'
 	unamestr=$(uname)
 	if [[ $unamestr == 'Linux' ]]; then
 		platform='linux'
-		
-		local pkgmgr='unknown'
+		pkgmgr='unknown'
+		ag='apt-get'
 		declare -A osInfo;
 		osInfo[/etc/redhat-release]=yum
 		osInfo[/etc/arch-release]=pacman
 		osInfo[/etc/gentoo-release]=emerge
 		osInfo[/etc/SuSE-release]=zypp
 		osInfo[/etc/debian_version]=apt-get
-		for f in ${!osInfo[@]}			
-		do
-			if [ -f $f ];then
-				brew = ${osInfo[$f]};
-				alias brew = ${osInfo[$f]};
+		for f in ${!osInfo[@]}; do
+			if [ -f $f ];then ag=${osInfo[$f]}; fi
+		done;
+		declare -A pkgs;
+		pkgs[git]=git
+		pkgs[ruby]=ruby
+		pkgs[rake]=rake
+		pkgs[zsh]=zsh
+		pkgs[vim]=vim
+		pkgs[lnav]=lnav
+		pkgs[htop]=htop
+		pkgs[tree]=tree
+		for i in ${pkgs[@]}; do
+			
+			if ! [ -x "$(command -v $i)" ]; then
+				sudo ${osInfo[$f]} install -y  $i
+			else
+				echo "-- $i already installed ---"
 			fi
+			
 		done
+		
 		
 		
 	elif [[ $unamestr == 'Darwin' ]]; then
@@ -30,27 +43,15 @@ if [ ! -d "$HOME/.gzn" ]; then
 		if [[ $? != 0 ]]; then
 			/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 		else
-			brew upgrade && brew upgrade && brew cleanup && brew prune && brew doctor
-			brew install macvim
+			echo "--- brew is already isntalled ---"		
 		fi
 	fi
 	
-		
-	if [ -d $pkgmgr ]; then
-		for $m in "${deps[@]}"
-		do				
-			hash $m 2> /dev/null || ({printf >&2 "unable to find $m \n attempting to install with $pkgmgr \n \n "} && brew install -y $m)	
-		done
-	fi
-	
-if
 	echo "Installing jesson's dot files to .gzn"
 	git clone --depth=1 https://github.com/jessonfoo/dotfiles.git "$HOME/.gzn"
 	gzn="$HOME/.gzn"
 	cd $gzn
-	[ "$1" = "ask" ] && export ASK="true"
 	rake install
 else
 	echo "the directory .gzn already exists, to install or update cd ~/.gzn && rake update"
 fi
-	
